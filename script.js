b2c5f6320ac26ff2ab4277a9122b78c500f4124b
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-08-03 12:49:40 +0530

    initial push

---
+++ script.js
@@ -0,0 +1,90 @@
+const rps =['ROCK','PAPER','SCISSOR']
+let humanScore=0,compscore=0;
+
+function getComputerChoice(){
+   let comSel = Math.floor(Math.random() * 3 );
+   return rps[comSel];
+
+}
+function valHumanChoice(text_inp =''){
+    return rps.includes(text_inp)
+}
+function getHumanChoice(){
+   let  humanSel= prompt("Make your Choice.1st comes rock then scissor JANKENPO !!").toUpperCase();    
+    while(!valHumanChoice(humanSel)){
+        humanSel = prompt("Invalid choice, Think Hooman think !").toUpperCase();
+    }
+    return humanSel;
+}
+function rpslogic(opt1,opt2){
+    if(opt1 === opt2) { 
+        console.log("Draw !!");
+        return 0;
+    }
+
+    if(opt1 ==='SCISSOR' && opt2==='PAPER'){
+        console.log("HUMAN'S WIN"); 
+        return 1;
+    }
+    if(opt1 ==='ROCK' && opt2==='SCISSOR'){
+        console.log("HUMAN'S WIN");
+         return 1;
+        }
+    if(opt1 ==='PAPER' && opt2==='ROCK'){
+        console.log("HUMAN'S WIN"); 
+        return 1;
+    }
+
+    if(opt1 ==='ROCK' && opt2==='PAPER'){
+        console.log("COMPUTER WINS !!"); 
+        return 2;
+    }
+    if(opt1 ==='PAPER' && opt2==='SCISSOR'){
+        console.log("COMPUTER WINS !!"); 
+        return 2;
+    }
+    if(opt1 ==='SCISSOR' && opt2==='ROCK'){
+        console.log("COMPUTER WINS !!"); 
+        return 2;
+    }
+
+}
+function playRound() {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    console.log('computer Chose ' + computerSelection)
+    console.log('Human Chose '+ humanSelection )
+    let result = rpslogic(humanSelection,computerSelection);
+    if(result === 0) alert(" Draw ");
+    if(result === 1 ) {
+        alert("round for Humans");
+        humanScore ++;
+    }
+    if(result === 2 ) {
+        alert("round for Machines");
+        compscore++;
+    }   
+
+}
+function game()  {
+    let round = 0;
+    while(round != 5){
+        console.log('Round ' + round)
+        playRound();
+        console.log("Humans : " + humanScore +"Computers : " + compscore);
+        round++;
+    }
+    if(humanScore > compscore) {
+        console.log("All hail the humans !!");
+
+    }
+    else if(humanScore < compscore){
+        console.log("we are doomed T_T T_T");
+    }
+    else{
+        console.log("Fair competition Machines");
+    }
+}
+
+game();
+  
